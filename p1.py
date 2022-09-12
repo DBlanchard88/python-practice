55f3211b9e755ed4ee2235ff5d5a914723b34d03
DBlanchard <89858058+DBlanchard@users.noreply.github.com>
2022-09-12 13:46:13 -0400

    Update p1.py

--- p1.py
+++ p1.py
@@ -3,5 +3,9 @@ is_tall = False
 
 if is_male and is_tall:
     print("you are a male and tall")
+elif is_male and not(is_tall):
+    print('you are a short male')
+elif not(is_male) and is_tall:
+    print('you are not a male but are tall')
 else:
     print("you are either not male or not tall or both")
